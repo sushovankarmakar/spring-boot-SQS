f1a73e54f4c2413806183fc12c2c719085d04e10
Sushovan Karmakar <sushovan.karmakar@randstadrisesmart.com>
2020-12-06 08:33:50 +0530

    Repository Created, Dependencies added

---
+++ src/test/java/com/example/springbootSQS/SpringBootSqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootSQS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
